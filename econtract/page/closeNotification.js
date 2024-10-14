43a989cb6c556947a478fb3e41c5bf082572e7d8
Hà Huy Hoàn <hahuyhoan25ngoc@gmail.com>
2024-10-14 16:49:43 +0700

    anh Long đẹp zai quá

---
+++ econtract/page/closeNotification.js
@@ -0,0 +1,22 @@
+const {By, until} = require('selenium-webdriver');
+const Logger = require('../../helper/logger');
+
+
+
+module.exports = async function closeNotification(driver) {
+    try {
+        const logger = new Logger('Close notification');
+        logger.start();
+
+        let closeNotification = await driver.wait(until.elementLocated(
+            By.css("button.o_notification_close.btn")),
+            2000
+        );
+
+        await closeNotification.click();
+        
+        logger.stop();
+    } finally {
+        
+    }
+}
--- econtract/run.js
+++ econtract/run.js
@@ -4,8 +4,8 @@ const switchCompany = require('./page/switchCompany');
 const addAdminUser = require('./page/addAdminUser');
 const addPlan = require('./page/addPlan');
 const addPlanToCompany = require('./page/addPlanToCompany');
-
-
+const addUser = require('./page/addUser')
+const config = require('../helper/config');
 
 
 module.exports = async function run() {
@@ -14,14 +14,18 @@ module.exports = async function run() {
     await driver.manage().window().maximize();
     try {
         await login(driver);
-        await addCompany(driver);
+        // await addCompany(driver);
         await switchCompany(driver);
-        await addAdminUser(driver);
-        await addPlan(driver);
-        await addPlanToCompany(driver);
+        // await addAdminUser(driver);
+        // await addPlan(driver);
+        // await addPlanToCompany(driver);
+        for (let index = 0; index < config.econtract.listUser.length; index++) {
+            const user = config.econtract.listUser[index];
+            await addUser(driver, user)
+        }
     }catch(e) {
         throw e;
     } finally {
         await driver.quit();
     }   
-}
+}   
